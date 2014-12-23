251af3ba4f9baa248b8447dadbc1b52c934fa74c
Oliver Krohn <InterAction@quantentunnel.de>
2014-12-23 22:47:54 +0100

    Update coffeemaker-payment-system-v0_6_1.ino

--- coffeemaker-payment-system-v0_6_1.ino
+++ coffeemaker-payment-system-v0_6_1.ino
@@ -6,6 +6,13 @@ coffeemaker to another, they have to be changed in the code, if necessary.
 Hardware used: Arduino Uno, 16x2 LCD I2C, "RDM 630" RFID reader 125 kHz, HC-05 bluetooth, buzzer, 
 male/female jumper wires, a housing.
 
+pinouts:
+Analog pins 4,5 - LCD I2C
+Digital pins 0,1 - Hardware Serial RX, TX(communicates with coffeemaker)
+Digital pins 2,3 - RFID RX, TX
+Digital pins 4,5 - Software Serial RX, TX (bluetooth)
+Digital pin 12 - piezo buzzer
+
 I used a RFID reader, but of course it would be possible to use NFC instead. Already existing cards can be used! Any RFID 
 (or NFC) tag can be registered. Registering of new cards, charging (up to 25,50 EUR by now, may be changed,
 too) and deleting of old cards is triggered via the Android app.´
@@ -22,9 +29,9 @@ LiquidCrystal_I2C lcd(0x27,16,2);
 #define error(s) error_P(PSTR(s))
 
 SoftwareSerial myBT(4,5); // RX, TX
-SoftwareSerial myCoffeemaker(10,11); // RX, TX
 
 int i;
+int j;
 int k; 
 const int n = 40;  // max total number of cards with access (up to 200 max!)
 
@@ -44,7 +51,7 @@ long int RFIDcards[n] = {
 int creditArray[n] = {};  
   
 // EEPROM related variables
-byte cardByte[n*6];
+byte cardByte[];
 long int cardNr;
 
 String BTstring="";
@@ -66,23 +73,19 @@ union{
 } 
 creditConvert;
 
-union{
-  byte priceByte[2];
-  int priceInt;  
-} 
-priceConvert;
-
 void setup()
 {
   lcd.init();
   lcd.backlight();
   lcd.print(F("CoffeemakerPS"));
+  lcd.setCursor(0,1);
+  lcd.print(F("v0.6.1.1"));
   delay(3500);
   lcd.clear();
   lcd.print(F("starting up"));
   Serial.begin(9600);         // start serial communication at 9600bps
-//  myBT.begin(9600);           // Bluetooth at 9600bps (default)
-//  myCoffeemaker.begin(9600);  // Coffeemaker serial communication
+  myBT.begin(9600);           // Bluetooth at 9600bps (default)
+//  Serial.begin(9600);  // Coffeemaker serial communication
   attachInterrupt(0, ISRreceiveData0, FALLING );  // RFID: data0/rx is connected to pin 2, which results in INT 0
   attachInterrupt(1, ISRreceiveData1, FALLING );  // RFID: data1/tx is connected to pin 3, which results in INT 1
   for (i = 0; i < n; i++){  // read card numbers and referring credit from EEPROM
@@ -94,19 +97,11 @@ void setup()
     creditConvert.creditByte[1] = EEPROM.read(i*6+5);
     RFIDcards[i]  = cardConvert.cardNr;  // union to put the four bytes together
     creditArray[i] = creditConvert.creditInt;
-    Serial.print(F("Card: ")); 
-    Serial.print(print10digits(RFIDcards[i]));  
-    Serial.print(F("\tCredit: ")); 
-    Serial.println(printCredit(creditArray[i]));
   }
   for (i = 0; i < 8; i++){
-    priceConvert.priceByte[0] = EEPROM.read(i*2+1000);
-    priceConvert.priceByte[1] = EEPROM.read(i*2+1001);
-    priceArray[i] = priceConvert.priceInt;
-    Serial.print("Product ");
-    Serial.print(i+1);
-    Serial.print(": ");
-    Serial.println(printCredit(priceArray[i]));
+    creditConvert.creditByte[0] = EEPROM.read(i*2+1000);
+    creditConvert.creditByte[1] = EEPROM.read(i*2+1001);
+    priceArray[i] = creditConvert.creditInt;
   }
     
   lcd.clear();
@@ -124,33 +119,28 @@ void loop()
   do {
     RFIDcard = RFID();
     if (RFIDcard > 0){
-      k = n;
-      Serial.print(F("Card number:\t"));
-      Serial.println(print10digits(RFIDcard));
-      for(int i=0;i<n;i=i++){         
+  //    k = n;
+      for(i=0;i<n;i=i++){         
         if (((RFIDcard) == (RFIDcards[i]))){  
-          Serial.print(printCredit(creditArray[i]));
-          Serial.println(F(" left"));
           lcd.backlight();
-          lcd.setCursor(0, 0);
+  //        lcd.setCursor(0, 0);
           lcd.print(print10digits(RFIDcard));
           lcd.setCursor(0, 1);
-          lcd.print(printCredit(creditArray[i]));
-          lcd.print(F(" left"));      
-          delay(2000);
-          lcd.noBacklight();
-          lcd.clear();
+          lcd.print(printCredit(creditArray[i]));      
           k = i;
           i = n;          
         }
       }
       if (k == n){ 
-        Serial.print(F("card "));
-        Serial.print(print10digits(RFIDcard));
-        Serial.println(F(" unknown!"));
+        lcd.print(print10digits(RFIDcard));
+		lcd.setCursor(0,1);
+        lcd.print(F("card unknown!"));		
         k=0; 
-        beep(3);
-      }   
+        beep(2);
+      } 
+      delay(2000);
+      lcd.noBacklight();
+      lcd.clear();	    
     }
   } 
   while ( (millis()-time) < 60 ); 
@@ -158,17 +148,18 @@ void loop()
 
 
 // Check if there is a bluetooth connection and command
-BTstring = BT();
+  while( myBT.available() ){  
+    BTstring += String(char(myBT.read()));
+      delay(1);
+  }
   
 if (BTstring.length() > 0){
-    Serial.print(F("String ")); 
-    Serial.println(BTstring);
 // BT: Registering new cards
     if( BTstring == "RRR" ){           // starts registering cards and stops if longer than 5 sec no card
       time = millis();
       beep(1);
         lcd.backlight();
-        lcd.setCursor(0,0);
+    //    lcd.setCursor(0,0);
         lcd.print(F("Registering"));
         lcd.setCursor(0,1);
         lcd.print(F("new cards"));
@@ -182,9 +173,6 @@ if (BTstring.length() > 0){
       k = 0;
       for(i=0;i<n;i++){
         if ((RFIDcard == RFIDcards[i]) && (RFIDcard > 0) && (k == 0)){   //  && (RFIDcard>0 (((RFIDcard) == (RFIDcards[i])) || ((card) > 0))
-          Serial.print(F("card number "));
-          Serial.print(print10digits(RFIDcard));
-          Serial.println(F(" already exists!"));
           lcd.clear();
           lcd.print(print10digits(RFIDcard));
           lcd.setCursor(0,1);
@@ -192,7 +180,7 @@ if (BTstring.length() > 0){
           i = n;
           k = 1;
           time = millis();           
-          beep(3);
+          beep(2);
         }
       }
       for(i=0;i<n;i++){    
@@ -201,9 +189,6 @@ if (BTstring.length() > 0){
           creditArray[i] = priceArray[7]; // each new card gets 10 EUR credit
           cardConvert.cardNr = RFIDcards[i];
           creditConvert.creditInt = creditArray[i];
-          Serial.print(F("card number "));
-          Serial.print(print10digits(RFIDcard));
-          Serial.println(F(" registered!"));
           lcd.clear();
           lcd.print(print10digits(RFIDcard));
           lcd.setCursor(0,1);
@@ -221,23 +206,21 @@ if (BTstring.length() > 0){
         }   
       }
     } while ( (millis()-time) < 10000 );
-    beep(1);
-    beep(2);
-    beep(1);    
+    beep(3);   
     lcd.clear();
     lcd.noBacklight();    
     }
 
 // BT: Send RFID card numbers to app    
   if(BTstring =="LLL"){  // 'L' for 'list' sends RFID card numbers to app   
-    for(byte i=0;i<n;i++){   
+    for(i=0;i<n;i++){   
       myBT.print(print10digits(RFIDcards[i]));
       if (i < n-1) myBT.write(',');   
     }
   }
   if(BTstring.startsWith("DDD") == true){
       BTstring.remove(0,3); // removes "DDD" and leaves the index
-      int i = BTstring.toInt();
+      i = BTstring.toInt();
       i--; // list picker index (app) starts at 1, while RFIDcards array starts at 0       
       EEPROM.write(i*6, 0);
       EEPROM.write(i*6+1, 0);
@@ -245,19 +228,16 @@ if (BTstring.length() > 0){
       EEPROM.write(i*6+3, 0);
       EEPROM.write(i*6+4, 0);
       EEPROM.write(i*6+5, 0);
-      Serial.print(F("Card number "));
-      Serial.print(print10digits(RFIDcards[i]));
-      Serial.print(F(" at index number "));
-      Serial.println(i);
-      Serial.print(F(" has been deleted!"));
       beep(1);
       lcd.backlight();
-      lcd.clear();
       lcd.print(print10digits(RFIDcards[i])); 
       lcd.setCursor(0,1);
       lcd.print(F("deleted!"));
       RFIDcards[i] = 0;
-      delay(1000); 
+      delay(2000);
+      lcd.clear();
+      lcd.noBacklight();
+      
   }
     
 // BT: Charge a card    
@@ -268,48 +248,31 @@ if (BTstring.length() > 0){
     char a3 = BTstring.charAt(5);  // 5 and 6 => value to charge
     char a4 = BTstring.charAt(6);    
     BTstring = String(a1)+String(a2); 
-    Serial.println(BTstring);
-    byte i = BTstring.toInt();    // index of card
+    i = BTstring.toInt();    // index of card
     BTstring = String(a3)+String(a4);
-    Serial.println(BTstring); 
     int j = BTstring.toInt();   // value to charge
     j *= 100;
     i--; // list picker index (app) starts at 1, while RFIDcards array starts at 0       
-    Serial.println(i);
-    Serial.println(j);
-    Serial.println(creditArray[i]);
-    Serial.println(creditArray[i]+j);
-    creditConvert.creditInt = (creditArray[i]+j);   
-//    creditConvert.creditInt = creditArray[i];
-    
+    creditConvert.creditInt = (creditArray[i]+j);
     EEPROM.write(i*6+4, creditConvert.creditByte[0]);
     EEPROM.write(i*6+5, creditConvert.creditByte[1]);
     
-    
-    Serial.print(F("Card number "));
-    Serial.print(print10digits(RFIDcards[i]));
-    Serial.print(F(" credit "));
-    Serial.print(printCredit(creditArray[i]));  // -j gelöscht
-    Serial.print(F(" +"));
-    Serial.print(printCredit(j));
-    Serial.print(F(" = "));
-    Serial.println(printCredit(creditArray[i]+j));
     beep(1);
     lcd.backlight();
-    lcd.clear();
+  //  lcd.clear();
     lcd.print(print10digits(RFIDcards[i])); 
     lcd.setCursor(0,1);
-    lcd.print(printCredit(creditArray[i]));
+    // lcd.print(printCredit(creditArray[i]));
     lcd.print(F("+"));
     lcd.print(printCredit(j));
-    delay(1000);
+    delay(2000);
     lcd.noBacklight();
     lcd.clear();
     creditArray[i] += j;
   }
 // BT: Change prices   
   if(BTstring.startsWith("CHA") == true){
-    // first 2 characters after "CHA" is the product number (e.g. "01" for small cup)
+    // write price list product 1 to 10 (0-9), prices divided by commas
     k = 3;
     for (i = 0; i < 8;i++){  
       String tempString = "";
@@ -318,37 +281,47 @@ if (BTstring.length() > 0){
         k++;
       } while (BTstring.charAt(k) != ','); 
       j = tempString.toInt();
-      Serial.println(tempString); 
-      priceConvert.priceInt = j;
-      EEPROM.write(i*2+1000, priceConvert.priceByte[0]);
-      EEPROM.write(i*2+1001, priceConvert.priceByte[1]);
+      creditConvert.creditInt = j;
+      EEPROM.write(i*2+1000, creditConvert.creditByte[0]);
+      EEPROM.write(i*2+1001, creditConvert.creditByte[1]);
       priceArray[i] = j;
       k++;
     }
   }
   
   if(BTstring.startsWith("REA") == true){  
+    // read price list product 1 to 10 (0-9), prices divided by commas
     for (i = 0; i < 8; i++) {
-      Serial.print(priceArray[i]);
-      Serial.print(",");
       myBT.print(priceArray[i]);
       if (i < 7) myBT.write(',');
     }
-    Serial.println();
   }  
   
-  if(BTstring == "?M3\r\n"){  
-    for (i = 0; i < 1024; i++) {    // write a zero to every single byte of the 1 kb EEPROM 
-      EEPROM.write(i, 0);
-    }
+  if(BTstring == "?M3"){  
+  //  for (i = 0; i < 1024; i++) {    // write a zero to every single byte of the 1 kb EEPROM 
+   //   EEPROM.write(i, 0);
+//    }
+    lcd.backlight();
     toCoffeemaker("?M3\r\n");  // activates incasso mode (= no coffee w/o "ok" from the payment system! May be inactivated by sending "?M3" without quotation marks)
+    beep(1);
+    lcd.print(F("Inkasso mode"));
+    lcd.setCursor(0,1);
+    lcd.print(F("activated!"));
+    delay(2000);
+    lcd.clear();
+    lcd.noBacklight();
   }
 
-  if(BTstring == "?M1\r\n"){  
-    for (i = 0; i < 1024; i++) {    // write a zero to every single byte of the 1 kb EEPROM 
-      EEPROM.write(i, 0);
-    }
+  if(BTstring == "?M1"){  
     toCoffeemaker("?M1\r\n");  // activates incasso mode (= no coffee w/o "ok" from the payment system! May be inactivated by sending "?M3" without quotation marks)
+    beep(1);
+    lcd.backlight();
+    lcd.print(F("Inkasso mode"));
+    lcd.setCursor(0,1);
+    lcd.print(F("deactivated!"));  
+    delay(2000);
+    lcd.clear();
+    lcd.noBacklight();    
   }    
   BTstring = ""; // Reset val that it is only read once  
 }
@@ -360,47 +333,38 @@ String message = fromCoffeemaker();   // gets answers from coffeemaker
 
   if (message.length() > 0){
  
-    if (message.charAt(0) == '?'){        // message command? (start with ?)
+    if (message.charAt(0) == '?' && message.charAt(1) == 'P'){        // message command? (start with ?)
+      lcd.backlight();
+      price = 0;
       if (message == "?PAE\r\n"){
         price = priceArray[0];        // product 1 (small cup)
-        Serial.println();
-        Serial.print(F("small cup\t"));
+        lcd.print(F("Small cup"));
       }     
       if (message == "?PAF\r\n"){
         price = priceArray[1];      // product 2 (2 small cups)
-        Serial.println();
-        Serial.print(F("2 small cups\t"));
+        lcd.print(F("2 small cups"));
       } 
       if (message == "?PAA\r\n"){         // large cup
         price = priceArray[2];   // product 3 (large cup)
-        Serial.println();
-        Serial.print(F("large cup\t"));
+        lcd.print(F("Large cup"));
       } 
       if (message == "?PAB\r\n"){
-        price = priceArray[31];     // product 4 (2 large cups)
-        Serial.println();
-        Serial.print(F("2 large cups\t"));
+        price = priceArray[3];     // product 4 (2 large cups)
+        lcd.print(F("2 large cups"));
       } 
       if (message == "?PAJ\r\n"){
         price = priceArray[4];      // product 5 (steam)
-        Serial.println();
-        Serial.print(F("Dampf 2\t"));
+        lcd.print(F("Steam 2"));
       } 
       if (message == "?PAI\r\n"){
         price = priceArray[5];      // product 6 (steam)
-        Serial.println();
-        Serial.print(F("Dampf 1\t"));
+        lcd.print(F("Steam 1"));
       }    
       if (message == "?PAG\r\n"){
         price = priceArray[6];      // product 7 (extra large cup)
-        Serial.println();
-        Serial.print(F("extra large cup\t"));
+        lcd.print(F("Extra large cup"));
       }  
-      else { 
-        Serial.print(message);
-      }
-      Serial.print("-");
-      Serial.println(printCredit(price/10));
+      lcd.println(printCredit(price/10));
   
   
       // RFID Identification      
@@ -411,78 +375,54 @@ String message = fromCoffeemaker();   // gets answers from coffeemaker
         if (RFIDcard > 0) break;   
       } 
       while ( (millis()-time) < 5000 );
-      Serial.print(F("Card number: \t"));
-      Serial.println(print10digits(RFIDcard));
-      for(int i=0;i<n;i=i++){         
-        if (((RFIDcard) == (RFIDcards[i])) && (RFIDcard>0 )){   // (((RFIDcard) == (RFIDcards[i])) || ((card) > 0))
-  //        k = i;
-  
-          // Enough credit?     
+      for(i=0;i<n;i=i++){         
+        if (((RFIDcard) == (RFIDcards[i])) && (RFIDcard>0 )){   // (((RFIDcard) == (RFIDcards[i])) || ((card) > 0)) 
           if ((creditArray[i] - price) > 0){
             creditArray[i] -= price;
-            Serial.println(printCredit(creditArray[i]));
             lcd.backlight();
             lcd.setCursor(0, 0);
             lcd.print(RFIDcard);
             lcd.setCursor(0, 1);
             lcd.print(printCredit(creditArray[i]));
-            lcd.print(F(" left"));
             creditConvert.creditInt = creditArray[i];
             EEPROM.write(i*6+4, creditConvert.creditByte[0]);
             EEPROM.write(i*6+5, creditConvert.creditByte[1]);
             toCoffeemaker(F("?ok\r\n"));          
-            delay(2000);
-            lcd.noBacklight();
-            lcd.clear();       
+   
           } 
-          else { 
-            Serial.println(printCredit(creditArray[i]));
-            Serial.println(F(" not enough credit"));
+          else {   // not enough credit!
             lcd.backlight();
             lcd.setCursor(0, 0);
             lcd.print(print10digits(RFIDcard));
             lcd.setCursor(0, 1);
             lcd.print(printCredit(creditArray[i]));
-            beep(3);
-            lcd.noBacklight();
-            beep(3);
-            lcd.backlight();
-            beep(3);
-            lcd.noBacklight();      
-            lcd.clear();    
+            beep(2);  
           }
+		// delay und backlight war hier   
         } 
         else {
           access = false;
         }
       }
+      delay(2000);
+      lcd.noBacklight();
+      lcd.clear();      
     }
   }
 }
 
-String BT(){
-  myBT.begin(9600);
-  delay(90);  // 100
-  while( myBT.available() ){  
-    BTstring += String(char(myBT.read()));
-      delay(60);
-  }
- return(BTstring); 
-  }
 
 String fromCoffeemaker(){
-  myCoffeemaker.begin(9600);
-  delay(10); // testweise
   String inputString = "";
   char d4 = 255;
-  while (myCoffeemaker.available()){    // if data is available to read
-    byte d0 = myCoffeemaker.read();
+  while (Serial.available()){    // if data is available to read
+    byte d0 = Serial.read();
     delay (1); 
-    byte d1 = myCoffeemaker.read();
+    byte d1 = Serial.read();
     delay (1); 
-    byte d2 = myCoffeemaker.read();
+    byte d2 = Serial.read();
     delay (1); 
-    byte d3 = myCoffeemaker.read();
+    byte d3 = Serial.read();
     delay (7);
     
     bitWrite(d4, 0, bitRead(d0,2));
@@ -506,13 +446,7 @@ String fromCoffeemaker(){
 
 void toCoffeemaker(String outputString)
 {
-  myCoffeemaker.begin(9600);
-  delay(10); 
-  Serial.println();
-  Serial.print(outputString);
-  Serial.println(outputString.length());
   for (byte a = 0; a < outputString.length(); a++){
-    Serial.print(outputString.charAt(a));
     byte d0 = 255;
     byte d1 = 255;
     byte d2 = 255;
@@ -526,13 +460,13 @@ void toCoffeemaker(String outputString)
     bitWrite(d3, 2, bitRead(outputString.charAt(a),6));  
     bitWrite(d3, 5, bitRead(outputString.charAt(a),7)); 
 
-    myCoffeemaker.write(d0); 
+    Serial.write(d0); 
     delay(1);
-    myCoffeemaker.write(d1); 
+    Serial.write(d1); 
     delay(1);
-    myCoffeemaker.write(d2); 
+    Serial.write(d2); 
     delay(1);
-    myCoffeemaker.write(d3); 
+    Serial.write(d3); 
     delay(7);
   }
 }
@@ -554,7 +488,7 @@ String printCredit(int credit){
 String print10digits(long int number) {
   String(tempString) = String(number);
   String(newString) = "";
-  int i = 10-tempString.length();
+  i = 10-tempString.length();
   for (int a = 0; a < (10-tempString.length()); a++){
     newString += "0";
   }
@@ -575,33 +509,31 @@ String print2digits(int number) {
 void beep(byte number){
   int duration = 200;
   switch (number) {
-    case 1: 
+    case 1: // positive feedback
     tone(12,1500,duration);
     delay(duration);
     break;
-    case 2:
-    tone(12,1000,duration);
-    delay(duration);
-    break;
-    case 3:
+    case 2: // negative feedback
     tone(12,500,duration);
     delay(duration);
-    break;
-    case 4: 
+    break;     
+    case 3:  // action stopped (e.g. registering) double beep
+    tone(12,1000,duration);
+    delay(duration);
+    tone(12,1500,duration);
+    delay(duration);    
+    break; 
+    case 4:  // alarm (for whatever)
     for (int a = 0; a < 3; a++){
-      for (int i = 2300; i > 600; i-=50){
-        Serial.print(i);
+      for (i = 2300; i > 600; i-=50){
           tone(12,i,20);
           delay(18);
       }     
-      for (int i = 600; i < 2300; i+=50){
-        Serial.print(i);
+      for (i = 600; i < 2300; i+=50){
           tone(12,i,20);
           delay(18);
       }
-    }
- 
-  break;  
+    }  
   }
 }
 
@@ -640,7 +572,7 @@ long int RFID(){
 }
 
 byte checkParity(){
-  int i = 0;
+  i = 0;
   int evenCount = 0;
   int oddCount = 0;
   for(i = 0; i < 8; i++){
@@ -695,6 +627,7 @@ void ISRreceiveData1(){
   recvBitCount++;
   isData1Low = 1;
 }
+
 
 
 
